151a99ad6f066c2d3d59cc1ea5d1655581d44813
Proneet Verma <proneet11@gmail.com>
2025-07-27 01:23:25 +0100

    fixup

---
+++ README.md
@@ -0,0 +1,4 @@
+```sh
+$ brew install rocksdb # for macos, and set the path in build.zig
+$ zig build run
+```
--- main.zig
+++ main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const c = @cImport({
+const rdb = @cImport({
     @cInclude("rocksdb/c.h");
 });
 
@@ -8,46 +8,45 @@ pub fn main() !void {
 
     var err: [*c]u8 = null;
 
-    const options = c.rocksdb_options_create();
-    defer c.rocksdb_options_destroy(options);
-    c.rocksdb_options_set_create_if_missing(options, 1);
-    c.rocksdb_options_set_error_if_exists(options, 1);
-    c.rocksdb_options_set_compression(options, c.rocksdb_snappy_compression);
+    const options = rdb.rocksdb_options_create();
+    defer rdb.rocksdb_options_destroy(options);
+    rdb.rocksdb_options_set_create_if_missing(options, 1);
+    rdb.rocksdb_options_set_compression(options, rdb.rocksdb_snappy_compression);
 
-    const db = c.rocksdb_open(options, db_name, &err);
+    const db = rdb.rocksdb_open(options, db_name, &err);
     if (err != null) {
         std.debug.print("open error: {s}\n", .{err});
-        c.rocksdb_free(err);
+        rdb.rocksdb_free(err);
         return;
     }
-    defer c.rocksdb_close(db);
+    defer rdb.rocksdb_close(db);
 
-    const wo = c.rocksdb_writeoptions_create();
-    defer c.rocksdb_writeoptions_destroy(wo);
+    const wo = rdb.rocksdb_writeoptions_create();
+    defer rdb.rocksdb_writeoptions_destroy(wo);
 
     const key = "name";
     const value = "foo";
 
-    c.rocksdb_put(db, wo, key.ptr, key.len, value.ptr, value.len, &err);
+    rdb.rocksdb_put(db, wo, key.ptr, key.len, value.ptr, value.len, &err);
     if (err != null) {
         std.debug.print("put error: {s}\n", .{err});
-        c.rocksdb_free(err);
+        rdb.rocksdb_free(err);
         return;
     }
 
-    const ro = c.rocksdb_readoptions_create();
-    defer c.rocksdb_readoptions_destroy(ro);
+    const ro = rdb.rocksdb_readoptions_create();
+    defer rdb.rocksdb_readoptions_destroy(ro);
 
     var val_len: usize = 0;
-    const val_ptr = c.rocksdb_get(db, ro, key.ptr, key.len, &val_len, &err);
-    defer if (val_ptr != null) c.rocksdb_free(val_ptr);
+    const val_ptr = rdb.rocksdb_get(db, ro, key.ptr, key.len, &val_len, &err);
+    defer if (val_ptr != null) rdb.rocksdb_free(val_ptr);
 
     if (err != null) {
         std.debug.print("get error: {s}\n", .{err});
-        c.rocksdb_free(err);
+        rdb.rocksdb_free(err);
         return;
     }
 
     const val = val_ptr[0..val_len];
-    std.debug.print("get key len: {}, value: {s}\n", .{ val_len, val });
+    std.debug.print("get key: {s}, len: {}, value: {s}\n", .{ key, val_len, val });
 }
